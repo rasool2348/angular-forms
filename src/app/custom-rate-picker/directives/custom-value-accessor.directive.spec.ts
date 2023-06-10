5616d7d845309dec48c2ea3137c23da3adf63c0e
r.kakaei <r.kakaei@irisaco.com>
2023-06-10 16:11:42 +0330

    remove test files

--- src/app/custom-rate-picker/directives/custom-value-accessor.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { CustomValueAccessorDirective } from './custom-value-accessor.directive';
-
-describe('CustomValueAccessorDirective', () => {
-  it('should create an instance', () => {
-    const directive = new CustomValueAccessorDirective();
-    expect(directive).toBeTruthy();
-  });
-});
